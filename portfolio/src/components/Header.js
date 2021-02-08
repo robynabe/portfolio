9fb27c1e282cc0114aed990af4a8fa83f9445f49
robynabe <64428921+robynabe@users.noreply.github.com>
2021-02-08 13:52:57 -0800

    Added brand logo

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,11 +1,12 @@
 // HEADER //
 import { Link } from 'react-router-dom';
 import Nav from './Nav';
+import logo from '../images/logo.png';
 
 function Header() {
   return (
     <header>
-      <h1><Link to={'/'}>Robyn Abe</Link></h1>
+      <Link to={'/'}><img className="logo" src={logo} /></Link>
       <Nav />
     </header>
   );
