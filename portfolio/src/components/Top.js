b827e75bd911b102c18cfe7318e65225eb983176
robynabe <64428921+robynabe@users.noreply.github.com>
2021-02-17 13:18:08 -0800

    Styled to top button

--- portfolio/src/components/Top.js
+++ portfolio/src/components/Top.js
@@ -7,7 +7,11 @@ function Top() {
 
   return (
     <a className="scroll-to-top" href="#header">
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z"/></svg>    
+      <div className="icon-container">
+        <svg className="triangle" xmlns="http://www.w3.org/2000/svg" width="28" height="25" viewBox="0 0 24 24"><path d="M24 22h-24l12-20z"/></svg>
+        <svg className="chevron one" width="25" height="25" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"/></svg>    
+        <svg className="chevron two" width="24" height="25" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"/></svg>
+      </div>
     </a>
   );
 }
