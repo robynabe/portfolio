57c80ab330bbc7347afc487415ce73c682d1f76d
robynabe <64428921+robynabe@users.noreply.github.com>
2021-03-01 21:34:08 -0800

    Google Analytics added to router

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import './styles/styles.scss';
+import ReactGA from 'react-ga';
+import Tracker from './router/tracker';
+
+const TRACKING_ID = "G-5SHTE8NYMX"; 
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Tracker />
     <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
+  
 );
 
 
