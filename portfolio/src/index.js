546fb01990b2dabd78a5dfa93f93cf6d6d799654
robynabe <64428921+robynabe@users.noreply.github.com>
2021-03-02 12:50:00 -0800

    Added accent font to match logo

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import './styles/styles.scss';
+import './font/Northwell.ttf';
 import ReactGA from 'react-ga';
 
 const TRACKING_ID = "G-5SHTE8NYMX"; 
